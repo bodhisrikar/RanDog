c7a9c3bb95fb06b4f22657b5c43dc7dada35d0c0
Srikar Thottempudi <srikarthottempudi@srikars-macbook-air.local>
2019-03-14 19:53:00 -0400

    Initial Commit

---
+++ RanDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RanDog
+//
+//  Created by Srikar Thottempudi on 3/14/19.
+//  Copyright © 2019 Srikar Thottempudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
